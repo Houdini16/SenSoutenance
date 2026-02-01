200342f5d27ceb2967f7d1782867f4211fcc6e4c
Aissatou-gueye <aissatougueye042001@gmail.com>
2026-02-01 17:25:23 +0000

    j'ai fais le design professeur

--- AppSenSoutenance/View/Paramètre/frmProfesseur.Designer.cs
+++ AppSenSoutenance/View/Paramètre/frmProfesseur.Designer.cs
@@ -1,20 +1,22 @@
 ﻿namespace AppSenSoutenance.View.Paramètre
 {
+    // Formulaire Professeur (vue WinForms)
     partial class frmProfesseur
     {
         /// <summary>
-        /// Required designer variable.
+        /// Conteneur des composants du formulaire
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        /// Libération des ressources utilisées par le formulaire
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">true si les ressources managées doivent être libérées</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
             {
+                // Libère les composants graphiques
                 components.Dispose();
             }
             base.Dispose(disposing);
@@ -23,11 +25,13 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        /// Méthode générée automatiquement par le Designer
+        /// Elle initialise tous les composants graphiques du formulaire
+        /// ⚠ Ne pas modifier manuellement
         /// </summary>
         private void InitializeComponent()
         {
+            // Déclaration et initialisation des contrôles
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button6 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
@@ -51,276 +55,112 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+
+            // Initialisation du DataGridView2
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.tabControl1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // comboBox1
-            // 
+
+            // ================= ComboBox1 =================
+            // Liste permettant d’afficher les candidatures ou sujets
             this.comboBox1.DisplayMember = "Lister les candidatures";
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(421, 52);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(378, 28);
-            this.comboBox1.TabIndex = 18;
             this.comboBox1.ValueMember = "Lister mes propositions ";
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(533, 443);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(145, 39);
-            this.button6.TabIndex = 17;
+
+            // ================= Boutons d’actions =================
+            // Bouton sélectionner une candidature
             this.button6.Text = "&Selectionner";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(814, 443);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(115, 39);
-            this.button5.TabIndex = 16;
+
+            // Bouton rejeter une candidature
             this.button5.Text = "&Rejeter";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(684, 443);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(115, 39);
-            this.button4.TabIndex = 15;
+
+            // Bouton accepter une candidature
             this.button4.Text = "&Accepter";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(417, 30);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(238, 20);
-            this.label4.TabIndex = 14;
+
+            // ================= Label =================
+            // Indique le type de liste affichée
             this.label4.Text = "Lister : Candidature / Sujet";
-            // 
-            // dataGridView2
-            // 
-            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(418, 95);
-            this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.RowHeadersWidth = 51;
-            this.dataGridView2.RowTemplate.Height = 24;
+
+            // ================= DataGridView2 =================
+            // Tableau affichant les candidatures ou sujets proposés
             this.dataGridView2.Size = new System.Drawing.Size(511, 342);
-            this.dataGridView2.TabIndex = 13;
-            // 
-            // btnRemove
-            // 
-            this.btnRemove.Location = new System.Drawing.Point(283, 398);
-            this.btnRemove.Name = "btnRemove";
-            this.btnRemove.Size = new System.Drawing.Size(115, 39);
-            this.btnRemove.TabIndex = 12;
+
+            // ================= Boutons CRUD =================
+            // Supprimer un sujet
             this.btnRemove.Text = "&Supprimer";
-            this.btnRemove.UseVisualStyleBackColor = true;
-            // 
-            // btnEdit
-            // 
-            this.btnEdit.Location = new System.Drawing.Point(283, 343);
-            this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(115, 39);
-            this.btnEdit.TabIndex = 11;
+
+            // Modifier un sujet
             this.btnEdit.Text = "&Modifier";
-            this.btnEdit.UseVisualStyleBackColor = true;
-            // 
-            // btnAdd
-            // 
-            this.btnAdd.Location = new System.Drawing.Point(283, 287);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(115, 39);
-            this.btnAdd.TabIndex = 10;
+
+            // Ajouter un nouveau sujet
             this.btnAdd.Text = "&Ajouter";
-            this.btnAdd.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(20, 118);
+
+            // ================= TextBox =================
+            // Champ texte pour saisir le sujet du mémoire
             this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(378, 163);
-            this.textBox1.TabIndex = 5;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 95);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 20);
-            this.label3.TabIndex = 4;
+
+            // ================= Labels =================
+            // Label du champ Sujet
             this.label3.Text = "Sujet";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 30);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(159, 20);
-            this.label2.TabIndex = 3;
+
+            // Label pour le choix de la filière
             this.label2.Text = "Choisir une filiere";
-            // 
-            // cbbDateSoutenance
-            // 
+
+            // ================= ComboBox =================
+            // Liste déroulante (filière ou date de soutenance)
             this.cbbDateSoutenance.FormattingEnabled = true;
-            this.cbbDateSoutenance.Location = new System.Drawing.Point(20, 52);
-            this.cbbDateSoutenance.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.cbbDateSoutenance.Name = "cbbDateSoutenance";
-            this.cbbDateSoutenance.Size = new System.Drawing.Size(378, 28);
-            this.cbbDateSoutenance.TabIndex = 2;
-            // 
-            // button7
-            // 
-            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button7.Location = new System.Drawing.Point(755, 447);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(168, 36);
-            this.button7.TabIndex = 6;
+
+            // ================= TabPage 1 : Mémoires =================
+            // Bouton de validation
             this.button7.Text = "&Valider";
-            this.button7.UseVisualStyleBackColor = true;
-            // 
-            // comboBox2
-            // 
+
+            // ComboBox pour filtrer ou sélectionner un mémoire
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(19, 48);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(326, 28);
-            this.comboBox2.TabIndex = 5;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(15, 23);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(182, 22);
-            this.label1.TabIndex = 4;
+
+            // Titre de la section
             this.label1.Text = "Memoire / Resumer";
-            // 
-            // button3
-            // 
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(386, 447);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(168, 36);
-            this.button3.TabIndex = 3;
+
+            // Bouton consulter un mémoire
             this.button3.Text = "&Consulter";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(570, 447);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(168, 36);
-            this.button2.TabIndex = 2;
+
+            // Bouton annoter un mémoire
             this.button2.Text = "&Annoter";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(772, 71);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(151, 36);
-            this.button1.TabIndex = 1;
+
+            // Bouton sélectionner un mémoire
             this.button1.Text = "&Selectionner";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage1
-            // 
-            this.tabPage1.Controls.Add(this.button7);
-            this.tabPage1.Controls.Add(this.comboBox2);
-            this.tabPage1.Controls.Add(this.label1);
-            this.tabPage1.Controls.Add(this.button3);
-            this.tabPage1.Controls.Add(this.button2);
-            this.tabPage1.Controls.Add(this.button1);
-            this.tabPage1.Controls.Add(this.dataGridView1);
-            this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1068, 569);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = " Memoires";
-            this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(19, 113);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
+
+            // DataGridView affichant la liste des mémoires
             this.dataGridView1.Size = new System.Drawing.Size(904, 325);
-            this.dataGridView1.TabIndex = 0;
-            // 
-            // tabControl1
-            // 
+
+            // ================= TabControl =================
+            // Contient deux onglets : Mémoires et Proposer un sujet
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(2, 2);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1076, 598);
-            this.tabControl1.TabIndex = 2;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Controls.Add(this.comboBox1);
-            this.tabPage2.Controls.Add(this.button6);
-            this.tabPage2.Controls.Add(this.button5);
-            this.tabPage2.Controls.Add(this.button4);
-            this.tabPage2.Controls.Add(this.label4);
-            this.tabPage2.Controls.Add(this.dataGridView2);
-            this.tabPage2.Controls.Add(this.btnRemove);
-            this.tabPage2.Controls.Add(this.btnEdit);
-            this.tabPage2.Controls.Add(this.btnAdd);
-            this.tabPage2.Controls.Add(this.textBox1);
-            this.tabPage2.Controls.Add(this.label3);
-            this.tabPage2.Controls.Add(this.label2);
-            this.tabPage2.Controls.Add(this.cbbDateSoutenance);
-            this.tabPage2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(954, 518);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Proposer un  Sujet";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // frmProfesseur
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(242)))), ((int)(((byte)(253)))));
+
+            // ================= Formulaire =================
+            // Paramètres généraux du formulaire Professeur
+            this.BackColor = System.Drawing.Color.FromArgb(227, 242, 253);
             this.ClientSize = new System.Drawing.Size(1080, 603);
             this.ControlBox = false;
-            this.Controls.Add(this.tabControl1);
-            this.Name = "frmProfesseur";
             this.Text = "Profésseur";
-            this.Load += new System.EventHandler(this.frmProfesseur_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
-            this.tabPage1.ResumeLayout(false);
-            this.tabPage1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            this.tabControl1.ResumeLayout(false);
-            this.tabPage2.ResumeLayout(false);
-            this.tabPage2.PerformLayout();
-            this.ResumeLayout(false);
 
+            // Événement déclenché au chargement du formulaire
+            this.Load += new System.EventHandler(this.frmProfesseur_Load);
+
+            // Fin de l’initialisation
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
         }
 
         #endregion
 
+        // Déclaration des composants privés du formulaire
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button5;
